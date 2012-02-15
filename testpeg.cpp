d6c6b3f51e34202a8fd73663d6d2b7dc8211f17a
Josh Volchko <josh.volchko@gmail.com>
2012-02-14 22:47:21 -0500

    testpeg.cpp

--- testpeg.cpp
+++ testpeg.cpp
@@ -1,16 +1,32 @@
-#include "peg.h"
+#include "Peg.h"
 
 int main()
 {
-	Peg peg = Peg();
-	while (peg.count() > 5) {
-		peg.remove();
-	}
-	peg.add(15);
-	peg.add(11);
-	peg.add(15);
-	peg.add(2);
-	std::cout << peg;
+	Peg bar = Peg(20);
+	Peg foo = Peg(30);
+	Peg baz = Peg(50);
+	baz.remove();
+	baz.remove();
 	
+	for (int i = 0; i < 21; i++) {
+		bar.remove();
+	}
+
+	for (int i = 20; i > 5; i--) {
+		bar.add(i);
+	}
+
+	std::cout << "Adding 0 to foo" << std::endl;
+	foo.add(0);
+
+	std::cout << "bar.topSize(): " << bar.topSize() << std::endl;
+	std::cout << "bar.count(): " << bar.count() << std::endl << std::endl;
+
+	std::cout << "foo.topSize(): " << foo.topSize() << std::endl;
+	std::cout << "foo.count(): " << foo.count() << std::endl << std::endl;
+
+	std::cout << "baz.topSize(): " << baz.topSize() << std::endl;
+	std::cout << "baz.count(): " << baz.count() << std::endl << std::endl;
+
 	return 0;
 }
